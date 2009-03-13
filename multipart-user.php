cc72336af6cef62df32659f255b5d066670fcca1
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-03-13 19:59:43 +0000

    add lib/multipart-user
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/lib/multipart-user/trunk@935 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ multipart-user.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Multipart User Form
+ * Author: Denis de Bernardy <http://www.mesoconcepts.com>
+ * Version: 1.0 alpha
+ */
+
+/**
+ * ob_multipart_user()
+ *
+ * @param string $buffer Output buffer
+ * @return string $buffer Modified buffer
+ **/
+
+function ob_multipart_user($buffer) {
+	return str_replace(
+		'<form id="your-profile"',
+		'<form enctype="multipart/form-data" id="your-profile"',
+		$buffer
+		);
+} # ob_multipart_user()
+
+ob_start('ob_multipart_user');
+?>
