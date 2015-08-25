7b9bbcb437537328387160529028782a9f04f336
Mike Koepke <mike.koepke@gmail.com>
2015-08-25 09:58:04 -0500

    Simply strip closing php ?> from the file

--- multipart-user.php
+++ multipart-user.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * Multipart User Form
- * Author: Denis de Bernardy <http://www.mesoconcepts.com>
- * Version: 1.0
+ * Author: Denis de Bernardy & Mike Koepke <http://www.semiologic.com>
+ * Version: 1.0.1
  */
 
 /**
@@ -30,7 +30,7 @@ if ( !function_exists('add_max_file_size') ) :
 
 function add_max_file_size() {
 	$bytes = apply_filters('import_upload_size_limit', wp_max_upload_size());
-	
+
 	echo  "\n" . '<input type="hidden" name="MAX_FILE_SIZE" value="' . esc_attr($bytes) .'" />' . "\n";
 } # add_max_file_size()
 endif;
@@ -39,4 +39,3 @@ ob_start('ob_multipart_user');
 
 add_action('show_user_profile', 'add_max_file_size');
 add_action('edit_user_profile', 'add_max_file_size');
-?>
